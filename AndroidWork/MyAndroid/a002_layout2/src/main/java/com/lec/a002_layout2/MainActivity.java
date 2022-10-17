a03b140b353778c11dc747c5eecbc3478cfea13c
Nappy97 <wjddmlgud829@gmail.com>
2022-10-17 19:53:51 +0900

    1017

---
+++ AndroidWork/MyAndroid/a002_layout2/src/main/java/com/lec/a002_layout2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lec.a002_layout2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear_gravity);
+    }
+}
